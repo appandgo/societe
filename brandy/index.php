d492d127e771e14de3790f13bf056b5799f82799
anthonyzakine <anthonyzakine@gmail.com>
2016-09-05 12:29:11 +0200

    change

--- brandy/index.php
+++ brandy/index.php
@@ -131,7 +131,7 @@
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAWyX7bxymlVnabFHYuH5bZ1NKM2xidGAI"></script>    
  <!--<script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script>-->
  
-    <script type='text/javascript'>function init_map(){var myOptions = {zoom:12,scrollwheel: false,center:new google.maps.LatLng(48.8205669,2.4236550000000534),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(48.8205669,2.4236550000000534)});infowindow = new google.maps.InfoWindow({content:'<strong>APP AND GO</strong><br>saint maurice<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+    <script type='text/javascript'>function init_map(){var myOptions = {zoom:12,scrollwheel: false,center:new google.maps.LatLng(48.8205669,2.4236550000000534),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(48.8205669,2.4236550000000534)});infowindow = new google.maps.InfoWindow({content:'<strong>APP AND GO</strong><br>Saint-Maurice<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
 
 </body>
 
