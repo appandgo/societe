b4418e5b80f0a9b5441ad6809a2c54722d0e59bb
mopiou <briansoufir4@hotmail.com>
2016-09-06 11:25:46 +0200

    Integration facebook

--- brandy/index.php
+++ brandy/index.php
@@ -86,6 +86,26 @@
 </head>
 
 <body>
+
+    <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1193258404072842',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+
     <!-- Preloader -->
     <div id="preloader">
         <div id="status">&nbsp;</div>
--- brandy/src/footer.php
+++ brandy/src/footer.php
@@ -17,8 +17,16 @@
                     <div class="social">
                         <h2>Suivez-nous</h2>
                         <ul class="icon_list">
-                            <li><a href="https://www.facebook.com/appandgo"target="_blank"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="https://www.instagram.com/appandgo/"target="_blank"><i class="fa fa-instagram"></i></a></li>
+                            <!--<li><a href="http://www.facebook.com/"target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="http://www.twitter.com/"target="_blank"><i class="fa fa-twitter"></i></a></li>-->
+                            <li>
+                                <div
+                                  class="fb-like"
+                                  data-share="true"
+                                  data-width="450"
+                                  data-show-faces="true">
+                                </div>
+                            </li>
                             <!--
                             <li><a href=""><i class="fa fa-google-plus"></i></a></li>
                             <li><a href=""><i class="fa fa-linkedin"></i></a></li>
