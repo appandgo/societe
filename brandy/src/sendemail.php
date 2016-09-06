8d716789cf091546d5f06f9f876ebce56f96122c
anthonyzakine <anthonyzakine@gmail.com>
2016-09-06 17:23:15 +0200

    email

--- brandy/src/sendemail.php
+++ brandy/src/sendemail.php
@@ -7,7 +7,7 @@ $subject    = @trim(stripslashes($_POST['subject']));
 $message    = @trim(stripslashes($_POST['message'])); 
 
 
-$to   		= "z-anthony@hotmail.fr";//replace with your email
+$to   		= "contact@app-and-go.fr";//replace with your email
 
 
 $headers   = array();
@@ -16,7 +16,7 @@ $headers[] = "Content-type: text/plain; charset=iso-8859-1";
 $headers[] = "From: {$name} <{$from}>";
 $headers[] = "Reply-To: <{$from}>";
 $headers[] = "Subject: {$subject}";
-$headers[] = "Bcc: <briansoufir4@gmail.com>";
+// $headers[] = "Bcc: <briansoufir4@gmail.com>";
 $headers[] = "X-Mailer: PHP/".phpversion();
 
 $result=mail($to, $subject, $message, implode("\r\n", $headers));
