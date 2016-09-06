a75cd3125f99d64885d516ec27fcdc93ea9e816c
mopiou <briansoufir4@hotmail.com>
2016-09-06 17:01:33 +0200

    og

--- brandy/index.php
+++ brandy/index.php
@@ -14,13 +14,16 @@
 
     <meta property="og:locale" content="fr_FR" />
     <meta property="og:type" content="website" />
+
+    <meta property="fb:app_id" content="1193258404072842" />
+    <meta property="fb:page_id" content="1395648730513184" /> 
+    
     <meta property="og:title" content="App and Go - Agence digitale" />
     <meta property="og:description" content="App and Go vous accompagne dans le développement de vos applications mobiles et site internet." />
     <meta property="og:url" content="http://www.app-and-go.fr" />
     <meta property="og:site_name" content="www.app-and-go.fr" />
     <meta property="og:image" content="http://www.app-and-go.fr/images/logo_appandgo/V2AppGoFuse18.png" /> 
-    <meta property="fb:app_id" content="1193258404072842" />
-    <meta property="fb:page_id" content="1395648730513184" /> 
+
     <!--<meta property="fb:admins" content="ID_FACEBOOK_DU_WEBMASTER" /> -->
 
 
