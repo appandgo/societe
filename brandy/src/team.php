063cc49b3ce425a0dff6ba2dfe985a4f44971329
anthonyzakine <anthonyzakine@gmail.com>
2016-09-07 10:41:16 +0200

    team changement

--- brandy/src/team.php
+++ brandy/src/team.php
@@ -2,15 +2,15 @@
     <div class="container">
       <div class="row">
         <div class="heading text-center col-sm-8 col-sm-offset-2 wow fadeInUp" data-wow-duration="1200ms" data-wow-delay="300ms">
-          <h2>The Team</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam</p>
+          <h2>L'equipe</h2>
+          <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam</p> -->
         </div>
       </div>
       <div class="team-members">
         <div class="row">
-          <div class="col-sm-3">
+          <div class="col-sm-4">
             <div class="team-member wow flipInY" data-wow-duration="1000ms" data-wow-delay="300ms">
-              <div class="member-image">
+              <div class="member-image zakine">
                 <img class="img-responsive" src="images/team/Anthonyprofil.jpg" alt="">
               </div>
               <div class="member-info">
@@ -20,11 +20,11 @@
               </div>
               <div class="social-icons">
                 <ul>
-                  <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                  <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                  <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                  <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-                  <li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li>
+                  <li><a class="facebook" href="https://www.facebook.com/zakine"><i class="fa fa-facebook"></i></a></li>
+                  <!-- <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li> -->
+                  <li><a class="linkedin" href="https://www.linkedin.com/in/anthony-zakine-28abbb114?trk=nav_responsive_tab_profile_pic"><i class="fa fa-linkedin"></i></a></li>
+                  <!-- <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+                  <li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li> -->
                 </ul>
               </div>
             </div>
@@ -50,9 +50,9 @@
               </div>
             </div>
           </div> -->
-          <div class="col-sm-3">
+          <div class="col-sm-4">
             <div class="team-member wow flipInY" data-wow-duration="1000ms" data-wow-delay="800ms">
-              <div class="member-image">
+              <div class="member-image soufir">
                 <img class="img-responsive" src="images/team/brianbolos.jpg" alt="">
               </div>
               <div class="member-info">
@@ -62,11 +62,11 @@
               </div>
               <div class="social-icons">
                 <ul>
-                  <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                  <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                  <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                  <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-                  <li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li>
+                  <li><a class="facebook" href="https://www.facebook.com/brian.soufir"><i class="fa fa-facebook"></i></a></li>
+                 <!--  <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li> -->
+                  <li><a class="linkedin" href="https://www.linkedin.com/in/brian-soufir-885473105?trk=pulse-det-athr_prof-art_hdr"><i class="fa fa-linkedin"></i></a></li>
+                 <!--  <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+                  <li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li> -->
                 </ul>
               </div>
             </div>
