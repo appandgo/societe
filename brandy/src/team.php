b92f0e9b8be85b93494dc68b5c06c755a01db30c
anthonyzakine <anthonyzakine@gmail.com>
2016-09-08 14:33:37 +0200

    changement texte perso

--- brandy/src/team.php
+++ brandy/src/team.php
@@ -2,8 +2,9 @@
     <div class="container">
       <div class="row">
         <div class="heading text-center col-sm-8 col-sm-offset-2 wow fadeInUp" data-wow-duration="1200ms" data-wow-delay="300ms">
-          <h2>L'equipe</h2>
-          <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam</p> -->
+          <h2>Equipe jeune et dynamique</h2>
+          
+          <p>Nous diversifions les talents pour augmenter la créativité et l'expertise technique au sein du même groupe.</p>
         </div>
       </div>
       <div class="team-members">
@@ -14,15 +15,15 @@
                 <img class="img-responsive" src="images/team/Anthonyprofil.jpg" alt="">
               </div>
               <div class="member-info">
-                <h3>Anthony Zakine</h3>
+                <h3>Anthony</h3>
                 <h4>CEO &amp; Founder</h4>
-                <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+               <!--  <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt</p> -->
               </div>
               <div class="social-icons">
                 <ul>
-                  <li><a class="facebook" href="https://www.facebook.com/zakine"><i class="fa fa-facebook"></i></a></li>
-                  <!-- <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li> -->
-                  <li><a class="linkedin" href="https://www.linkedin.com/in/anthony-zakine-28abbb114?trk=nav_responsive_tab_profile_pic"><i class="fa fa-linkedin"></i></a></li>
+                 <!-- <li><a class="facebook" href="https://www.facebook.com/zakine"><i class="fa fa-facebook"></i></a></li>
+                   <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li> 
+                  <li><a class="linkedin" href="https://www.linkedin.com/in/anthony-zakine-28abbb114?trk=nav_responsive_tab_profile_pic"><i class="fa fa-linkedin"></i></a></li>-->
                   <!-- <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
                   <li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li> -->
                 </ul>
@@ -56,15 +57,15 @@
                 <img class="img-responsive" src="images/team/brianfou2.jpg" alt="">
               </div>
               <div class="member-info">
-                <h3>Brian Soufir</h3>
+                <h3>Brian</h3>
                 <h4>Developer</h4>
-                <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+               <!--  <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt</p> -->
               </div>
               <div class="social-icons">
                 <ul>
-                  <li><a class="facebook" href="https://www.facebook.com/brian.soufir"><i class="fa fa-facebook"></i></a></li>
-                 <!--  <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li> -->
-                  <li><a class="linkedin" href="https://www.linkedin.com/in/brian-soufir-885473105?trk=pulse-det-athr_prof-art_hdr"><i class="fa fa-linkedin"></i></a></li>
+                  <!--<li><a class="facebook" href="https://www.facebook.com/brian.soufir"><i class="fa fa-facebook"></i></a></li>
+                   <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li> 
+                  <li><a class="linkedin" href="https://www.linkedin.com/in/brian-soufir-885473105?trk=pulse-det-athr_prof-art_hdr"><i class="fa fa-linkedin"></i></a></li>-->
                  <!--  <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
                   <li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li> -->
                 </ul>
@@ -77,14 +78,14 @@
                 <img class="img-responsive" src="images/team/leo.jpeg" alt="">
               </div>
               <div class="member-info">
-                <h3>Léonard Boisseau</h3>
+                <h3>Léonard</h3>
                 <h4>UI/UX Designer</h4>
-                <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+               <!--  <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt</p> -->
               </div>
               <div class="social-icons">
                 <ul>
-                  <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                  <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>                  
+                  <!-- <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                  <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   -->                
                 </ul>
               </div>
             </div>
