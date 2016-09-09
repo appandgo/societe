727cd1e34ea32839bdfdcaf711c7b7327f323938
anthonyzakine <anthonyzakine@gmail.com>
2016-09-09 11:28:07 +0200

    changement service et photo brian

--- brandy/src/service2.php
+++ brandy/src/service2.php
@@ -18,7 +18,7 @@
             <div class="service-info">
               
               <h3>CREATION GRAPHIQUE</h3>
-              <p>Identité visuelle /  Charte graphique  /  Webdesign  <br></p>
+              <p>Identité visuelle /  Charte graphique  / Logo <br>  Webdesign  <br></p>
             </div>
           </div>
           <div class="col-sm-4 wow fadeInDown socka" data-wow-duration="100ms" data-wow-delay="300ms">
@@ -36,8 +36,9 @@
               <i class="fa fa-globe"></i>
             </div>
             <div class="service-info">
-              <h3>WEB MARKETING</h3>
-              <p>Référencement naturel(SEO) / Référencement payant(SEM)  / Community Management </p>
+              <!-- <h3>WEB MARKETING</h3> -->
+              <h3>REFERENCEMENT</h3>
+              <p>SEO - Référencement naturel <br>SEA - référencement Google Adwords <br>SMO - Optimisation par les réseaux sociaux</p>
             </div>
           </div>
           <div class="col-sm-4 wow fadeInUp socka" data-wow-duration="100ms" data-wow-delay="400ms">
