87584e493f1e9702e6f9c15b6d626026adf60ca5
anthonyzakine <anthonyzakine@gmail.com>
2016-09-09 18:59:57 +0200

    og img

--- brandy/index.php
+++ brandy/index.php
@@ -32,8 +32,8 @@
     <meta name="twitter:title"        content="APP and GO - Agence digitale">
 
     
-    <link rel="icon" href="images/logo_appandgo/logoapp-and-go-google.png" sizes="32x32">
-    <link rel="icon" href="images/logo_appandgo/logoapp-and-go-google.png" sizes="192x192">
+    <link rel="icon" href="images/logo_appandgo/leo.jpeg" sizes="32x32">
+    <link rel="icon" href="images/logo_appandgo/leo.jpeg" sizes="192x192">
 
     <link rel="apple-touch-icon-precomposed" href="images/logo_appandgo/V2AppGo380.png">
     <meta name="msapplication-TileImage" href="images/logo_appandgo/V2AppGo380.png">
