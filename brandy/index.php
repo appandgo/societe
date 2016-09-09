2f357b49df114c8af6c41695569d58840503b486
anthonyzakine <anthonyzakine@gmail.com>
2016-09-09 12:25:46 +0200

    change og

--- brandy/index.php
+++ brandy/index.php
@@ -28,10 +28,12 @@
     <meta name="twitter:title"        content="APP and GO - Agence digitale">
 
     
-    <!-- <link rel="icon" href="images/logo_appandgo/V2AppGoFuse18.png" sizes="32x32">
+    <link rel="icon" href="images/logo_appandgo/V2AppGoFuse18.png" sizes="32x32">
     <link rel="icon" href="images/logo_appandgo/V2AppGoFuse18.png" sizes="192x192">
+
     <link rel="apple-touch-icon-precomposed" href="images/logo_appandgo/V2AppGo380.png">
-    <meta name="msapplication-TileImage" href="images/logo_appandgo/V2AppGo380.png"> -->
+    <meta name="msapplication-TileImage" href="images/logo_appandgo/V2AppGo380.png">
+    
     <link rel="shortcut icon" href="images/logo_appandgo/V2AppGoFuse18.png">
 
 
