6debe608476fa9e2976595f426137b5724458dd6
anthonyzakine <anthonyzakine@gmail.com>
2016-09-09 12:23:32 +0200

    change icon

--- brandy/index.php
+++ brandy/index.php
@@ -28,10 +28,11 @@
     <meta name="twitter:title"        content="APP and GO - Agence digitale">
 
     
-    <link rel="icon" href="images/logo_appandgo/V2AppGoFuse18.png" sizes="32x32">
+    <!-- <link rel="icon" href="images/logo_appandgo/V2AppGoFuse18.png" sizes="32x32">
     <link rel="icon" href="images/logo_appandgo/V2AppGoFuse18.png" sizes="192x192">
     <link rel="apple-touch-icon-precomposed" href="images/logo_appandgo/V2AppGo380.png">
-    <meta name="msapplication-TileImage" href="images/logo_appandgo/V2AppGo380.png">
+    <meta name="msapplication-TileImage" href="images/logo_appandgo/V2AppGo380.png"> -->
+    <link rel="shortcut icon" href="images/logo_appandgo/V2AppGoFuse18.png">
 
 
     <?php include_once("analyticstracking.php") ?>
@@ -82,7 +83,6 @@
   <link id="css-preset" href="css/presets/preset1.css" rel="stylesheet">
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
 <!--   <link rel="shortcut icon" href="images/rocket.png">-->
-<link rel="shortcut icon" href="images/logo_appandgo/V2AppGoFuse18.png">
 
    <link href="css/lightbox.css" rel="stylesheet">
 <!--   <link href="css/animate.min.css" rel="stylesheet"> --> 
